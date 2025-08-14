d791252636fa16d71fda581aeff478945c9296ee
Francisco Gomes <fgsj.dev@outlook.com>
2025-08-14 15:21:09 -0300

    feat: division of two numbers

---
+++ python-programmer/structure_sequential/exercise05.py
@@ -0,0 +1,5 @@
+number_one = int(input("number one: "))
+number_two = int(input("number two: "))
+division_numbers = number_one / number_two
+print("\n*** solution ***")
+print("\nThe division of two numbers is: %d" % division_numbers)
