700ec3aec5285a206483352051232c6df7d4fd73
Francisco Gomes <fgsj.dev@outlook.com>
2025-08-14 16:01:16 -0300

    feat: calculate average of three numbers

---
+++ python-programmer/structure_sequential/exercise09.py
@@ -0,0 +1,6 @@
+number_one = int(input("number one: "))
+number_two = int(input("number two: "))
+number_three = int(input("number three: "))
+average_three_numbers = (number_one + number_two + number_three) / 3
+print("\n*** Solution ***")
+print("\nThe average three numbers: %d" % average_three_numbers)
