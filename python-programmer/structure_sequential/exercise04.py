9e4c9a74a8141fc2af8751c971caab9c5ba6ccf6
Francisco Gomes <fgsj.dev@outlook.com>
2025-08-14 15:17:43 -0300

    feat:produtct of two numbers

---
+++ python-programmer/structure_sequential/exercise04.py
@@ -0,0 +1,5 @@
+number_one = int(input("number one: "))
+number_two = int(input("number two: "))
+product_two_numbers = number_one * number_two
+print("\n*** Solution ***")
+print("\nProduct of two numbers: %d" % product_two_numbers)
