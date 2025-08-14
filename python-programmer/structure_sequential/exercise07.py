98a36a6829f922c6ae55c5f7525e1b5031ea429e
Francisco Gomes <fgsj.dev@outlook.com>
2025-08-14 15:50:30 -0300

    feat: calculate sum of three numbers

---
+++ python-programmer/structure_sequential/exercise07.py
@@ -0,0 +1,6 @@
+number_one = int(input("number one: "))
+number_two = int(input("number two: "))
+number_three = int(input("number three: "))
+sum_of_three_numbers = number_one + number_two + number_three
+print("\n*** Solution ***")
+print("\nThe sum of three numbers: %d" % sum_of_three_numbers)
