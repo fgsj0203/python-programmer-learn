02d14ddf2e28a6d4958267c243d2ed3ad48859f4
Francisco Gomes <fgsj.dev@outlook.com>
2025-08-14 15:45:39 -0300

    feat: calculating average of two numbers

---
+++ python-programmer/structure_sequential/exercise06.py
@@ -0,0 +1,5 @@
+number_one = int(input("number one: "))
+number_two = int(input("number two: "))
+average_numbers = (number_one + number_two) / 2
+print("\n*** Solution ***")
+print("\nThe average of two numbers: %d" % average_numbers)
