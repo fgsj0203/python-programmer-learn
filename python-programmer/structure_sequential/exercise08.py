64e24eb251e2caa44895ea032d9a67712c2d4b0d
Francisco Gomes <fgsj.dev@outlook.com>
2025-08-14 15:57:16 -0300

    feat: calculate product of three numbers

---
+++ python-programmer/structure_sequential/exercise08.py
@@ -0,0 +1,6 @@
+number_one = int(input("number one: "))
+number_two = int(input("number two: "))
+number_three = int(input("number three: "))
+product_three_numbers = number_one * number_two * number_three
+print("\n*** Solution ***")
+print("\nThe product of three numbers: %d" % product_three_numbers)
