c4e11ed908a18714dee0a3456464b1ccc185ad3f
Francisco Gomes <fgsj.dev@outlook.com>
2025-08-18 16:01:05 -0300

    exercise 17: printing number one elevate a number two

---
+++ python-programmer/structure_sequential/exercise17.py
@@ -0,0 +1,8 @@
+import math
+
+number_one = int(input("number one: "))
+number_two = int(input("number two: "))
+number_pow = math.pow(number_one, number_two)
+
+print("\n*** Solution ***")
+print("\nNumber one elevate a number two: %.1f" % number_pow)
