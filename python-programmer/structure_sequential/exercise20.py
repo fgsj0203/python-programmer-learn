8f72938f7fd38041d373fcfbca32dd13cb453f03
Francisco Gomes <fgsj.dev@outlook.com>
2025-08-18 19:09:19 -0300

    feat: difference absolute of two numbers

---
+++ python-programmer/structure_sequential/exercise20.py
@@ -0,0 +1,10 @@
+import math
+
+number_one = int(input("number one: "))
+number_two = int(input("number two: "))
+
+difference_number = number_one - number_two
+absolute_number = abs(difference_number)
+
+print("\n*** Solution ***")
+print("\nThe difference absolute is: %d" % absolute_number)
